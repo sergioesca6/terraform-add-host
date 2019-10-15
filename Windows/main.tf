314780c4b93d966de926fc50291981e5dfb640e8
Sergio Escalante <macuser@SergioEscalante.local>
2019-10-14 23:04:01 -0400

    Add variables to variables terraform file

--- Windows/main.tf
+++ Windows/main.tf
@@ -1,26 +1,3 @@
-variable "connection_type" {
-  description = "defines the tipe of connection being used to connect to vm"
-}
-
-variable "user_name" {
-    description = "username used for vm"
-}
-
-variable "password" {
-    description = "password used for vm"
-}
-
-variable "host_ip" {
-    description = "ip address of vm"
-}
-
-variable "ip_address_add" {
-    description = "ip address to add to hosts file"
-}
-variable "hostname_add" {
-    description = "hostname to add to hosts file"
-}
-
 resource "null_resource" "vm_configuration" {
   connection {
       type     = "${var.connection_type}"
