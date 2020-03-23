8ed9aa9e1bb3686c0f1f0421525a3d4873634bec
Sergio Escalante <sergioescalante62@gmail.com>
2020-03-23 14:55:34 -0400

    Update test template to apply to either Linux or Windows regardless of OS

--- test/main.tf
+++ test/main.tf
@@ -10,38 +10,22 @@ resource "null_resource" "vm_configuration_linux" {
   }
   provisioner "file" {
       content = <<EOF
-      #! /bin/bash
-      ip_address="${var.ip_address_add}"
-      hostname="${var.hostname_add}"
-      host_entry= "$ip_address   $hostname"
-
-      echo "Adding new host entry..."
-      echo "$host_entry" >> /etc/hosts
+      with open("/etc/hosts", "a+") as file_object:
+        file_object.seek(0)
+        data = file_object.read(100)
+        if len(data) > 0 :
+          file_object.write("\n")
+        file_object.write("hello hi")
+        file_object.close()
 
       EOF
-      destination = "/tmp/addshost.sh"
+      destination = "/tmp/addshost.py"
   }
 
   # Execute the script remotely
   provisioner "remote-exec" {
     inline = [
-      "chmod +x /tmp/addhost.sh; bash /tmp/addhost.sh"
+      "python /tmp/addhost.py"
     ]
   }
-}
-
-resource "null_resource" "vm_configuration_windows" {
-  connection {
-      type     = "${var.connection_type}"
-      user     = "${var.user_name}"
-      password = "${var.password}"
-      host     = "${var.host_ip}"
-  }
-	# Execute the script remotely
-  provisioner "remote-exec" {
-    inline = [  
-    "powershell.exe \"Add-Content -path C:\\windows\\System32\\drivers\\etc\\hosts -value '@{var.ip_address_add}   ${var.hostname_add}'\""
-    ]
-  }
-
 }
