a69830fc40ed4f9186c16bc5a803ab8d4e9320c8
Sergio Escalante <sergioescalante62@gmail.com>
2020-03-24 21:32:53 -0400

    Change to just one main.tf and variables.tf for the terraform template

--- main.tf
+++ main.tf
@@ -29,3 +29,30 @@ resource "null_resource" "vm_configuration_linux" {
     ]
   }
 }
+
+# ---------lINUX---------
+# provisioner "file" {
+#       content = <<EOF
+#       #! /bin/bash
+#       ip_address="${var.ip_address_add}"
+#       hostname="${var.hostname_add}"
+#       host_entry= "$ip_address   $hostname"
+#       echo "Adding new host entry..."
+#       echo "$host_entry" >> /etc/hosts
+#       EOF
+#       destination = "/tmp/addshost.sh"
+#   }
+#   # Execute the script remotely
+#   provisioner "remote-exec" {
+#     inline = [
+#       "chmod +x /tmp/addhost.sh; bash /tmp/addhost.sh"
+#     ]
+#   }
+
+
+#---------WINDOWS----------
+# provisioner "remote-exec" {
+#     inline = [  
+#     "powershell.exe \"Add-Content -path C:\\windows\\System32\\drivers\\etc\\hosts -value '@{var.ip_address_add}   ${var.hostname_add}'\""
+#     ]
+#   }
